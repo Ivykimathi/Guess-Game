41aa4db09cecf1e88dce71116d4f44f8140e1cec
Ivy <kajujuivy7872@gmail.com>
2023-06-28 17:29:47 +0300

    first

---
+++ guessgame/GuessGame.java
@@ -0,0 +1,13 @@
+
+package guessgame;
+
+public class GuessGame {
+player p1;
+player p2;
+player p3;
+  
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ guessgame/startGame.java
@@ -0,0 +1,12 @@
+
+package guessgame;
+
+public class startGame {
+    GuessGame game = new GuessGame();
+    game.startGame();
+    p1 = new player;
+    p2 = new player;
+    p3 = new player;
+    
+    
+}
